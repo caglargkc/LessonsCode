62b8b51b035ddd4cad308f6afb8b541d9955830e
Çağlar Gökçe <caglargokce@outlook.com.tr>
2023-12-17 13:11:41 +0300

    add

---
+++ Cem Boz - React - 2023/taskApp/src/components/TaksList.jsx
@@ -0,0 +1,20 @@
+import TaskShow from "./TaskShow";
+
+function TaskList({ tasks, onDelete, onUpdate }) {
+  return (
+    <div className="task-list">
+      {tasks.map((task, index) => {
+        return (
+          <TaskShow
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default TaskList;
